90f4668514e8c25a7182ff9890799b885ea3a195
Tim Hårek Andreassen <tim@harek.no>
2022-07-05 21:29:48 +0200

    fix: columncolor to string not number
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- .config/nvim/lua/sets.lua
+++ .config/nvim/lua/sets.lua
@@ -18,6 +18,7 @@ vim.o.list = true
 vim.o.scrolloff = 8
 vim.o.sidescrolloff = 8
 vim.o.signcolumn = 'yes'
+vim.o.colorcolumn = '80'
 vim.o.background = 'dark'
 vim.o.listchars = 'tab:▸ ,trail:·'
 vim.o.wildignore = vim.o.wildignore .. '*/tmp/*,*.so,*.swp,*.zip,public/'
