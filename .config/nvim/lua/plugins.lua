bcbd7e5bb72e90062a05b98df064780c9be09116
Tim Hårek Andreassen <tim@harek.no>
2022-11-26 12:24:15 +0100

    chore(nvim): Update api call
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -67,20 +67,20 @@ cmp.setup({
 })
 
 require'lspconfig'.pyright.setup {
-  capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 }
 
 require'lspconfig'.tailwindcss.setup {
-  capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 }
 
 
 require'lspconfig'.yamlls.setup {
-  capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 }
 
 require'lspconfig'.denols.setup {
-  capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 }
 
 --Enable (broadcasting) snippet capability for completion
