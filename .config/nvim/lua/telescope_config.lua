74ee1e1644034ef2bb26051eedb76a53c2ca4eac
Tim Hårek Andreassen <tim@harek.no>
2023-01-02 22:13:08 +0100

    chore(nvim): New bindings for telescope
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- .config/nvim/lua/telescope_config.lua
+++ .config/nvim/lua/telescope_config.lua
@@ -1,15 +1,26 @@
 -- Telescope
-vim.api.nvim_set_keymap('n', '<C-p>', "<cmd>lua require('telescope.builtin').git_files()<CR>", { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>gs', "<cmd>lua require('telescope.builtin').git_status()<CR>", { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<CR>", { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>fs', "<cmd>lua require('telescope.builtin').grep_string()<CR>", { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>fg', "<cmd>lua require('telescope.builtin').live_grep()<CR>", { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>fh', "<cmd>lua require('telescope.builtin').help_tags()<CR>", { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>gd', "<cmd>lua require('telescope.builtin').lsp_references()<CR>", { noremap = true })
-vim.api.nvim_set_keymap('n', 'z=', "<cmd>lua require('telescope.builtin').spell_suggest()<CR>", { noremap = true })
+-- See `:help telescope.builtin`
+vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>/', function()
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, { desc = '[/] Fuzzily search in current buffer]' })
+
+vim.keymap.set('n', '<leader>gs', require('telescope.builtin').git_status, { desc = '[G]it [S]tatus' })
+vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [F]iles' })
+vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', 'z=', require('telescope.builtin').spell_suggest, { desc = 'Spell suggest' })
+vim.keymap.set('n', '<C-p>', require('telescope.builtin').git_files, { desc = 'Open git-project file browser' })
 vim.api.nvim_set_keymap(
-  "n",
-  "<space>fb",
+  'n',
+  '<leader>fb',
   "<cmd>lua require 'telescope'.extensions.file_browser.file_browser()<CR>",
   {noremap = true}
 )
