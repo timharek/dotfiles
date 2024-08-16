def0a1f396a0b6a18d251194552b72f61f0d1e5e
Tim Hårek Andreassen <tim@harek.no>
2024-08-16 21:44:24 +0200

    feat: Add wezterm config
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,16 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- For example, changing the color scheme:
+config.color_scheme = 'OneDark (base16)'
+config.font = wezterm.font 'JetBrains Mono'
+config.font_size = 16.0
+
+config.enable_tab_bar = false
+config.window_decorations = 'RESIZE'
+
+-- and finally, return the configuration to wezterm
+return config
